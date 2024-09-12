139627abcfcc3aa824fba7a95ce4480fdf73b67a
William Guilherme Dahlke <williamgd@weg.net>
2024-09-12 17:08:36 -0300

    Added more metrics to save on prometheus

--- app/src/routes/routes.js
+++ app/src/routes/routes.js
@@ -5,8 +5,31 @@ let register = prom.register;
 
 const usuariosOnline = new prom.Gauge({
     name: 'gis_usuarios_online_total',
-    help: 'Número de usuários logados no momento'
+    help: 'Número de usuários logados no momento',
+    labelNames: ['unity']
 });
+
+const tempoSalvarProjeto = new prom.Histogram({
+    name: 'gis_tempo_salvarproj_segundos',
+    help: 'Tempo em segundos que o gis levou para salvar o projeto',
+    buckets: [100, 300, 500, 800, 1000, 3000, 5000, 8000]
+});
+
+const calculaMinutosToMs = (arr) => arr.map(num=> num * 60000);
+const arrayTempoGerarProj = calculaMinutosToMs([15, 20, 25, 30, 35, 40, 45, 50, 55, 60]);
+
+const tempoGerarProjeto = new prom.Histogram({
+    name: 'cm_tempo_gerarproj_minutos',
+    help: 'Tempo em minutos que o CM leva para gerar o projeto',
+    buckets: arrayTempoGerarProj
+});
+
+const tempoOperacoesSap = new prom.Histogram({
+    name: 'gis_tempo_op_sap_segundos',
+    help: 'Tempo em segundos que o GIS levou para realizar as integrações com o SAP',
+    buckets: [100, 300, 500, 800, 1000, 3000, 5000, 8000, 10000],
+    labelNames: ['operacao']
+})
 
 /**
  * @swagger
@@ -78,27 +101,45 @@ router.post('/metrics/insert', function (req, res) {
     
     const metricType = req.body.Type;
     switch (metricType){
-        case 1:            
-            break;
         case 2:
             setGaugeValue(req.body);
             break;
         case 3:
+            setHistogramValue(req.body);
             break;
         default:
             res.status(400).send("Metric type isn't registered.");
     }
-    res.status(200).send("Finalizou");
+    res.status(200).send();
 });
 
 function setGaugeValue(requestBody){
+    const labelsBody = requestBody.Labels;
     if (requestBody.MetricName == "gis_usuarios_online_total"){
         if (requestBody.Operation == 1){
-            usuariosOnline.inc(1);
+            usuariosOnline.labels(labelsBody[0]).inc();
         } 
         else if (requestBody.Operation == 2){
-            usuariosOnline.dec(1);
-        }        
+            usuariosOnline.labels(labelsBody[0]).dec();
+        }
+    }
+}
+
+function setHistogramValue(requestBody){
+    const labelsBody = requestBody.Labels;
+    
+    switch(requestBody.MetricName){
+        case "gis_tempo_salvarproj_segundos":
+            tempoSalvarProjeto.observe(requestBody.ElapsedTimeMs);
+            break;
+        case "cm_tempo_gerarproj_minutos":
+            tempoGerarProjeto.observe(requestBody.ElapsedTimeMs);
+            break;
+        case "gis_tempo_op_sap_segundos":
+            tempoOperacoesSap.labels(labelsBody[0]).observe(requestBody.ElapsedTimeMs);
+            break;
+        default:
+            break;
     }
 }
 
