a193ab4510d9383c7184f167eca3f6e793a5fefb
William Guilherme Dahlke <williamgd@weg.net>
2024-09-13 07:20:38 -0300

    Updated code to create docker image

--- app/swagger.js
+++ app/swagger.js
@@ -1,6 +1,6 @@
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const swaggerOptions = require('../swagger.json')
+const swaggerOptions = require('./swagger.json')
 
 module.exports = (app) => {
   app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc(swaggerOptions)));
