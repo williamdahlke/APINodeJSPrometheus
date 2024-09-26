25f36282ca0cb9072a7e50e53dbcf217c95040a8
William Guilherme Dahlke <williamgd@weg.net>
2024-09-26 13:47:42 -0300

    Fixed swagger details

--- app/src/routes/routes.ts
+++ app/src/routes/routes.ts
@@ -34,7 +34,7 @@ router.get('/metrics', async (req: Request, res: Response) => {
 
 /**
  * @swagger
- * /api/metrics/insert:
+ * /api/metrics/insert/gauge:
  *  post:
  *    summary: Post metrics to integrate with prometheus
  *    tags: [Metrics]
@@ -47,11 +47,17 @@ router.get('/metrics', async (req: Request, res: Response) => {
  *             properties:
  *               MetricName:
  *                 type: string
+ *               Help:
+ *                 type: string
  *               Type:
  *                 type: integer
  *               Operation:
  *                 type: integer
- *               Labels:
+ *               LabelNames:
+ *                 type: array
+ *                 items:
+ *                  type: string
+ *               LabelValues:
  *                 type: array
  *                 items:
  *                  type: string
@@ -64,40 +70,29 @@ router.get('/metrics', async (req: Request, res: Response) => {
  *                    type: string
  *             example:
  *               MetricName: gis_usuarios_online_total
+ *               Help: Número de usuários logados no momento
  *               Type: 2
  *               Operation: 1
- *               Labels: ["WTD_BNU"]
+ *               LabelNames: ["unity"]
+ *               LabelValues: ["WTD-BNU"]
  *               User:
  *                Name: williamgd
- *                Unity: WTD_BNU
+ *                Unity: WTD-BNU
  *    responses:
  *      201:
  *        description: Metrics registered successfully
  *      400:
  *        description: An error occurred or JSON is invalid
  */
-router.post('/metrics/insert', (req: Request, res: Response) => {
+router.post('/metrics/insert/gauge', (req: Request, res: Response) => {
 
   if (!isValidJson(req.body)) {
     return res.status(400).send("JSON is not valid.");
   }
   
-  switch (req.body.Type ){
-    case 2:
-      const gaugeMetric : GaugeMetric = req.body;
-      gaugeMetric.User = new WegUser(req.body.User.Name, req.body.User.Unity); 
-      setGaugeMetric(gaugeMetric);
-      break;
-
-    case 3:
-      const histogramMetric : HistogramMetric = req.body;
-      histogramMetric.User = new WegUser(req.body.User.Name, req.body.User.Unity);     
-      setHistogramMetric(histogramMetric);
-      break;
-
-    default:
-      return res.status(400).send("Metric type isn't registered.");      
-  }
+  const gaugeMetric : GaugeMetric = req.body;
+  gaugeMetric.User = new WegUser(req.body.User.Name, req.body.User.Unity); 
+  setGaugeMetric(gaugeMetric);
   res.status(201).send();
 });
 
@@ -105,6 +100,75 @@ function setGaugeMetric(metric: GaugeMetric) {
   addUpdateGauge(metric);
   setActiveUserGaugeMetric(metric);
 }
+
+/**
+ * @swagger
+ * /api/metrics/insert/histogram:
+ *  post:
+ *    summary: Post metrics to integrate with prometheus
+ *    tags: [Metrics]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               MetricName:
+ *                 type: string
+ *               Help:
+ *                 type: string                               
+ *               Type:
+ *                 type: integer
+ *               Buckets:
+ *                 type: array
+ *                 items:
+ *                  type: integer
+ *               LabelNames:
+ *                 type: array
+ *                 items:
+ *                  type: string
+ *               LabelValues:
+ *                 type: array
+ *                 items:
+ *                  type: string
+ *               ElapsedTimeMs:
+ *                 type: long
+ *               User:
+ *                 type: object
+ *                 properties:
+ *                  Name:
+ *                    type: string
+ *                  Unity:
+ *                    type: string
+ *             example:
+ *               MetricName: gis_tempo_op_sap_segundos
+ *               Help: Tempo em segundos que o GIS levou para realizar as integrações com o SAP
+ *               Type: 3   
+ *               Buckets: [100,300,500,800,1000,3000,5000,8000,10000]
+ *               LabelNames: ["operation"]
+ *               LabelValues: ["Checkin"]
+ *               ElapsedTimeMs: 1155
+ *               User:
+ *                Name: williamgd
+ *                Unity: WTD-BNU
+ *    responses:
+ *      201:
+ *        description: Metrics registered successfully
+ *      400:
+ *        description: An error occurred or JSON is invalid
+ */
+router.post('/metrics/insert/histogram', (req: Request, res: Response) => {
+
+  if (!isValidJson(req.body)) {
+    return res.status(400).send("JSON is not valid.");
+  }
+  
+  const histogramMetric : HistogramMetric = req.body;
+  histogramMetric.User = new WegUser(req.body.User.Name, req.body.User.Unity);     
+  setHistogramMetric(histogramMetric);
+  res.status(201).send();
+});
 
 function setHistogramMetric(metric: HistogramMetric) {
   addUpdateHistogram(metric);
